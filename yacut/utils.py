193c83815e06e812f88f8584ce20a45b5a3a0025
Павел Филиппов <pgphil@yandex.ru>
2024-01-19 18:37:21 +0300

    Correct after first review.

--- yacut/utils.py
+++ yacut/utils.py
@@ -9,11 +9,11 @@ def get_unique_short_id():
     """
     Creates a unique id.
     """
-    source_symbols = string.ascii_letters + string.digits 
+    source_symbols = string.ascii_letters + string.digits
     max_attempts = 10
     for _ in range(max_attempts):
         short_link = ''.join(random.choice(source_symbols)
-                             for _ in range(LEN_OF_DEFAULT_LINK)) 
-        if not URLMap.query.filter_by(short=short_link).first(): 
+                             for _ in range(LEN_OF_DEFAULT_LINK))
+        if not URLMap.query.filter_by(short=short_link).first():
             return short_link
     raise Exception(f'Невозможно сгенерировать id после {max_attempts}.')
